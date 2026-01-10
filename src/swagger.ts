56b7af429a067029e431bef7b721f1eb076c0db4
Wesley DuSell <dusellfam@gmail.com>
2026-01-09 22:59:14 -0500

    INIT

--- src/swagger.ts
+++ src/swagger.ts
@@ -23,7 +23,7 @@ async function generate() {
 
   console.log('🧩 [openapi] Creating Swagger document...');
   const config = new DocumentBuilder()
-    .setTitle('NGX seed-service-nestjs API')
+    .setTitle('NGX document API')
     .setVersion('1.0.0')
     .build();
 
